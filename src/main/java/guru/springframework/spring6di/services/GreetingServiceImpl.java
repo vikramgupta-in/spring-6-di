d80e0130bdd4b6934e5e28906f1a3574e58d2f58
VikramG <VikramG@boxer.co.za>
2023-08-31 16:09:41 +0200

    5-di-with-spring

--- src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
@@ -1,6 +1,10 @@
 package guru.springframework.spring6di.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
+
     @Override
     public String sayGreeting() {
         return "Hello Everyone From Base Service!!!";
