b2af7ef3612021ac5ecfa47a21e5b66c355c3da5
VikramG <VikramG@boxer.co.za>
2023-08-31 23:38:16 +0200

    6-primary-beans

--- src/test/java/guru/springframework/spring6di/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/ConstructorInjectedControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Primary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
